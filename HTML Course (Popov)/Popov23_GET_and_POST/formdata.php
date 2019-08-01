0abd968bb28d6321bba4821efca08d6f8570ad2d
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-01 16:35:47 -0400

    GET and POST

---
+++ HTML Course (Popov)/Popov23_GET_and_POST/formdata.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 5;
+echo $a;
+
+//echo $_GET['password'];
+//echo $_GET['name'];
+//echo $_GET['sleep'];
+
+echo $_POST['password'];
+echo $_POST['name'];
+echo $_POST['sleep'];
+
+?>
