430acc5b626d8c55986d9ade8b4c4a0c561cba72
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-10-28 16:38:07 -0400

    Reorganizing files and folders.

---
+++ JavaScript for Kids (Morgan)/Capter02_DataTypes.js
@@ -0,0 +1,14 @@
+
+/*
+Type in Firefox to start a console
+    about:blank
+    CTRL + SHIFT + I
+*/
+
+// Draw as many cats as you want
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+}
+drawCats(10); // You can put any number of cats instead of 10.
