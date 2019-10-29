8d623f1add9ef1de49c660759959c1aba8e5ab96
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-10-28 22:47:49 -0400

    Added examples to chapter 2 code.

--- JavaScript for Kids (Morgan)/Chapter02_DataTypes.js
+++ JavaScript for Kids (Morgan)/Chapter02_DataTypes.js
@@ -5,10 +5,22 @@ Type in Firefox to start a console
     CTRL + SHIFT + I
 */
 
-// Draw as many cats as you want
-var drawCats = function (howManyTimes) {
-    for (var i = 0; i < howManyTimes; i++) {
-        console.log(i + " =^.^=");
-    }
-}
-drawCats(10); // You can put any number of cats instead of 10.
+/***** Chapter 2: Data Types and Variables *****/
+
+5; //Number
+"Hi, I'm a string" // string
+true; // boolean
+
+99 * 123;
+"This is a long string".slice(0, 4);
+true && false;
+
+12345 / 250;
+8 / ( 1+ 3);
+
+// VARIABLES (key word "var")
+
+var nick;
+var age = 12; // returns undefined (no value)
+age; // returns 12
+age = 13; // returns 13 as we are not creating a new variable
