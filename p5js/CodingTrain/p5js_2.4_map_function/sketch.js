4a4629067cae5f046f1e25cb352c603b1ac23d04
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2020-10-18 23:53:56 -0400

    Map function maps different ranges.

--- p5js/CodingTrain/p5js_2.4_map_function/sketch.js
+++ p5js/CodingTrain/p5js_2.4_map_function/sketch.js
@@ -1,46 +1,17 @@
 
 //Global variables
-var r = 218;
-var g = 160;
-var b = 221;
-
-var x1 = 100;
-var car = {type:"Fiat", model:"500", color:"white"};
-
-// define JavaScript object(s)
-let circle1 = {
-    x: 70,
-    y: 200,
-    diameter: 50
-};
-
-// define JavaScript object(s)
-let circle2 = {
-    x: 170,
-    y: 300,
-    diameter: 70
-};
-
+var red = 0;
+var blue = 255;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(r, g, b);
-  fill(250, 200, 200)
-
-  //standars varable usage with no object
-  ellipse(x1, 100, 80, 80); // circle (no object)
-  x1 += 1;
-
-  //Using the defined "circle(s)" object
-  ellipse(circle1.x, circle1.y, circle1.diameter, circle1.diameter);
-  circle1.x = circle1.x + 1;
-
-  fill(255, 255, 0);
-  circle(circle2.x, circle2.y, circle2.diameter);
-
-  //print(car.type); //pirt to console (F12)
+  red = map(mouseX, 0, 600, 0, 255); //direct mapping for red color
+  blue = map(mouseX, 0, 600, 255, 0); //reverse mapping for blue color
+  background(red, 0, blue);
+  fill(250, 118, 222);
+  ellipse(mouseX, 200, 64, 64);
 
 }
